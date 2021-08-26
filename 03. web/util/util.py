fdb0dc3acbb8e37eb9ffd32f82024923b5856caf
unknown <hjkl4428@gmail.com>
2021-08-26 23:54:05 +0900

    opgg

--- 03. web/util/util.py
+++ 03. web/util/util.py
@@ -467,7 +467,8 @@ def champion_statistics(lane, champion):
         soup.select('.champion-overview__stats.champion-overview__stats--pick')[2].find('strong').text,
         soup.select('.champion-overview__stats.champion-overview__stats--pick')[2].find('span').text,
         soup.select('.champion-overview__stats.champion-overview__stats--win')[2].find('strong').text],
-        'trend_items' : trend_items
+        'trend_items' : trend_items,
+        'counters' : [translation_champion(champ.text.replace('\n','').replace('\t\t\t\t\t\t\t\t\t','')) for champ in soup.select('.champion-stats-header-matchup__table__champion')]
     }
     
 

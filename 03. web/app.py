2bd7a0a73179d959315a2b5bc3a380724f398d2c
unknown <hjkl4428@gmail.com>
2021-10-29 23:48:57 +0900

    re

--- 03. web/app.py
+++ 03. web/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, session
 from riot_api.riot_api import riot_api
 from opgg.opgg import opgg_crawling
-import os, json
+import os
+import json
 import pandas as pd
 from util.util import *
 
@@ -10,14 +11,17 @@ app = Flask(__name__)
 app.secret_key = 'qwert12345'
 app.config['SESSION_COOKIE_PATH'] = '/'
 
+
 app.register_blueprint(riot_api, url_prefix='/riotapi')
 app.register_blueprint(opgg_crawling, url_prefix='/opgg')
 
+
 @app.route('/')
 def index():
-    menu = {'ho':1}
-    
+    menu = {'ho': 1}
+
     return render_template('main.html', menu=menu, version=check_version())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- 03. web/opgg/opgg.py
+++ 03. web/opgg/opgg.py
@@ -7,14 +7,14 @@ opgg_crawling = Blueprint('opgg', __name__)
 
 
 champion_df, item_df, spell_df, rune_df = new_datas()
-menu = {'ho':0, 'ri' :0, 'op' : 1, 'ris' : 0, 'opg' : 1}
+menu = {'ho': 0, 'ri': 0, 'op': 1, 'ris': 0, 'opg': 1}
 version = check_version()
 
 
-
 @opgg_crawling.route('/statistics', methods=['GET', 'POST'])
-def statistics(): 
-    img_link = 'http://ddragon.leagueoflegends.com/cdn/' + str(version) + '/img/champion/'
+def statistics():
+    img_link = 'http://ddragon.leagueoflegends.com/cdn/' + \
+        str(version) + '/img/champion/'
     lanes = ['TOP', 'JUNGLE', 'MID', 'ADC', 'SUPPORT']
     champion_dict, winRate_dict, pickRate_dict, tier_dict = tier_list()
     top_count = len(champion_dict['TOP'])
@@ -24,26 +24,28 @@ def statistics():
     sup_count = len(champion_dict['SUPPORT'])
     eng_champion_dict = {}
     for lane in lanes:
-        eng_champion_dict[lane] = [translationChampion2(champion) for champion in champion_dict[lane]]
-    return render_template('opgg/statistics.html', menu=menu, version= version,
-    img_link=img_link, champion_dict=champion_dict, winRate_dict=winRate_dict, pickRate_dict=pickRate_dict,
-    tier_dict=tier_dict, eng_champion_dict=eng_champion_dict, top_count=top_count, jg_count=jg_count, mid_count=mid_count,
-    adc_count=adc_count, sup_count=sup_count)
+        eng_champion_dict[lane] = [translationChampion2(
+            champion) for champion in champion_dict[lane]]
+    return render_template('opgg/statistics.html', menu=menu, version=version,
+                           img_link=img_link, champion_dict=champion_dict, winRate_dict=winRate_dict, pickRate_dict=pickRate_dict,
+                           tier_dict=tier_dict, eng_champion_dict=eng_champion_dict, top_count=top_count, jg_count=jg_count, mid_count=mid_count,
+                           adc_count=adc_count, sup_count=sup_count)
+
 
 @opgg_crawling.route('/statistics/<lane>/<champion>')
-def championStatistics(lane,champion):
+def championStatistics(lane, champion):
     lane = lane
     champion = champion
-    data_dict = champion_statistics(lane,champion)
-    kor_lane_dict = {'top' : '탑', 'jungle' : '정글', 'mid' : '미드', 'adc' : '원딜', 'support' : '서포터'}
+    data_dict = champion_statistics(lane, champion)
+    kor_lane_dict = {'top': '탑', 'jungle': '정글',
+                     'mid': '미드', 'adc': '원딜', 'support': '서포터'}
     kor_lane = kor_lane_dict[lane]
     kor_champion = translation_champion(champion)
     #champion = champion.capitalize()
-    
-    
+
     if champion == 'Cassiopeia':
         return render_template('opgg/champion_statistics(cassiopeia).html', menu=menu, version=version,
-        kor_lane=kor_lane, kor_champion=kor_champion, data_dict=data_dict, champion=champion)
+                               kor_lane=kor_lane, kor_champion=kor_champion, data_dict=data_dict, champion=champion)
     else:
         return render_template('opgg/champion_statistics.html', menu=menu, version=version,
-        kor_lane=kor_lane, kor_champion=kor_champion, data_dict=data_dict, champion=champion)
+                               kor_lane=kor_lane, kor_champion=kor_champion, data_dict=data_dict, champion=champion)
